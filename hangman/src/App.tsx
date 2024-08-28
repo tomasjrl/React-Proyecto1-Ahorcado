78e5d5a40f8f2a54809827defde1807411363f49
tomasjrl <tstabilini@gmail.com>
2024-08-28 00:34:06 -0300

    comienzo de proyecto

---
+++ hangman/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "Center",
+      }}
+    >
+      <div style={{ fontSize: "2 rem", textAlign: "center" }}>Lose Win</div>
+    <HangmanDrawing />
+    {/* <HangmanWord />
+    <Keyboard /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ hangman/src/HangmanDrawing.tsx
@@ -0,0 +1,15 @@
+export function HangmanDrawing() {
+  return (
+    <div style={{ position: "relative" }}>
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div style={{ height: "10px", width: "250px", background: "black" }} />
+    </div>
+  );
+}
